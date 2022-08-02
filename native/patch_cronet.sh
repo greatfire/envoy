bf8d7bf33820d1ec8806884f0d66dbdc99055e8d
Steven McDonald <steven.mcdonald@gmail.com>
2022-08-02 13:28:30 -0700

    remove the 6, 7, and 9 patches that were old browser conversion stuff

--- native/patch_cronet.sh
+++ native/patch_cronet.sh
@@ -13,15 +13,7 @@ patch --fuzz=0 --no-backup-if-mismatch --forward --strip=1 --force <"$PATCH_DIR/
 patch --fuzz=0 --no-backup-if-mismatch --forward --strip=1 --force <"$PATCH_DIR/0004-Add-host-map-rules-for-envoy-scheme.patch"
 # disabled cipher suites
 patch --fuzz=0 --no-backup-if-mismatch --forward --strip=1 --force <"$PATCH_DIR/0005-Add-disabled-cipher-suites-parameter.patch"
-# as feature
-patch --fuzz=0 --no-backup-if-mismatch --forward --strip=1 --force <"$PATCH_DIR/0006-Add-as-features-and-android-preferences.patch"
-
-patch --fuzz=0 --no-backup-if-mismatch --forward --strip=1 --force <"$PATCH_DIR/0007-Add-cmdline-switch.patch"
 patch --fuzz=0 --no-backup-if-mismatch --forward --strip=1 --force <"$PATCH_DIR/0008-Set-host-header-for-http2.patch"
-
-# FAILED
-#patch --fuzz=0 --no-backup-if-mismatch --forward --strip=1 --force <"$PATCH_DIR/0009-Add-setting-page-for-envoy-url.patch"
-
 
 patch --fuzz=0 --no-backup-if-mismatch --forward --strip=1 --force <"$PATCH_DIR/0010-Disable-external-intent.patch"
 patch --fuzz=0 --no-backup-if-mismatch --forward --strip=1 --force <"$PATCH_DIR/0011-Add-salt-parameter.patch"
