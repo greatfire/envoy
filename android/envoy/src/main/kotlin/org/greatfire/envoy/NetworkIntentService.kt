6421457ca0b3953c688937fdd60deb14de09c188
Steven McDonald <steven.mcdonald@gmail.com>
2023-05-12 16:27:10 -0700

    add a comment about the STUN server list

--- android/envoy/src/main/kotlin/org/greatfire/envoy/NetworkIntentService.kt
+++ android/envoy/src/main/kotlin/org/greatfire/envoy/NetworkIntentService.kt
@@ -691,6 +691,8 @@ class NetworkIntentService : IntentService("NetworkIntentService") {
     ) {
 
         val uri = URI(url)
+        // borrowed from the list Tor Browser uses
+        // https://gitlab.torproject.org/tpo/applications/tor-browser-build/-/merge_requests/617/diffs
         val ice = "stun:stun.l.google.com:19302,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478"
         val brokerUrl = ""
         val front = ""
