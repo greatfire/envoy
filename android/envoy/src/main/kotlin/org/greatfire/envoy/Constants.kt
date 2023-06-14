c1bda5e70c003c80db768d854e5b094c9382516a
Matthew N Bogner <mnbogner@pop-os.localdomain>
2023-06-14 11:19:16 -0700

    moved constants into their own class and added util class to obfuscate urls.  also fixed issue with checking direct urls.

---
+++ android/envoy/src/main/kotlin/org/greatfire/envoy/Constants.kt
@@ -0,0 +1,57 @@
+package org.greatfire.envoy
+
+    // Defines submit url action
+    const val ACTION_SUBMIT = "org.greatfire.envoy.action.SUBMIT"
+
+    // Defines arguments for submit url action
+    const val EXTRA_PARAM_SUBMIT = "org.greatfire.envoy.extra.PARAM_SUBMIT"
+    const val EXTRA_PARAM_DIRECT = "org.greatfire.envoy.extra.PARAM_DIRECT"
+    const val EXTRA_PARAM_CERT = "org.greatfire.envoy.extra.PARAM_CERT"
+    const val EXTRA_PARAM_SOURCES = "org.greatfire.envoy.extra.PARAM_SOURCES"
+    const val EXTRA_PARAM_INTERVAL = "org.greatfire.envoy.extra.PARAM_INTERVAL"
+    const val EXTRA_PARAM_START = "org.greatfire.envoy.extra.PARAM_START"
+    const val EXTRA_PARAM_END = "org.greatfire.envoy.extra.PARAM_END"
+    const val EXTRA_PARAM_FIRST = "org.greatfire.envoy.extra.PARAM_FIRST"
+
+    // Defines custom broadcasts
+    const val ENVOY_BROADCAST_VALIDATION_SUCCEEDED = "org.greatfire.envoy.VALIDATION_SUCCEEDED"
+    const val ENVOY_BROADCAST_VALIDATION_FAILED = "org.greatfire.envoy.VALIDATION_FAILED"
+    const val ENVOY_BROADCAST_BATCH_SUCCEEDED = "org.greatfire.envoy.BATCH_SUCCEEDED"
+    const val ENVOY_BROADCAST_BATCH_FAILED = "org.greatfire.envoy.BATCH_FAILED"
+    const val ENVOY_BROADCAST_UPDATE_SUCCEEDED = "org.greatfire.envoy.UPDATE_SUCCEEDED"
+    const val ENVOY_BROADCAST_UPDATE_FAILED = "org.greatfire.envoy.UPDATE_FAILED"
+    const val ENVOY_BROADCAST_VALIDATION_CONTINUED = "org.greatfire.envoy.VALIDATION_CONTINUED"
+    const val ENVOY_BROADCAST_VALIDATION_ENDED = "org.greatfire.envoy.VALIDATION_ENDED"
+
+    // Defines data for custom broadcasts
+    const val ENVOY_DATA_URL_SUCCEEDED = "org.greatfire.envoy.URL_SUCCEEDED"
+    const val ENVOY_DATA_URL_FAILED = "org.greatfire.envoy.URL_FAILED"
+    const val ENVOY_DATA_SERVICE_SUCCEEDED = "org.greatfire.envoy.SERVICE_SUCCEEDED"
+    const val ENVOY_DATA_SERVICE_FAILED = "org.greatfire.envoy.SERVICE_FAILED"
+    const val ENVOY_DATA_URL_LIST = "org.greatfire.envoy.URL_LIST"
+    const val ENVOY_DATA_SERVICE_LIST = "org.greatfire.envoy.SERVICE_LIST"
+    const val ENVOY_DATA_UPDATE_URL = "org.greatfire.envoy.UPDATE_URL"
+    const val ENVOY_DATA_UPDATE_LIST = "org.greatfire.envoy.UPDATE_LIST"
+    const val ENVOY_DATA_VALIDATION_MS = "org.greatfire.envoy.VALIDATION_MS"
+    const val ENVOY_DATA_VALIDATION_ENDED_CAUSE = "org.greatfire.envoy.VALIDATION_ENDED_CAUSE"
+
+    // Defines values for custom broadcasts
+    const val ENVOY_SERVICE_UPDATE = "update"
+    const val ENVOY_SERVICE_DIRECT = "direct"
+    const val ENVOY_SERVICE_HTTPS = "https"
+    const val ENVOY_SERVICE_ENVOY = "envoy"
+    const val ENVOY_SERVICE_V2WS = "v2ws"
+    const val ENVOY_SERVICE_V2SRTP = "v2srtp"
+    const val ENVOY_SERVICE_V2WECHAT = "v2wechat"
+    const val ENVOY_SERVICE_HYSTERIA = "hysteria"
+    const val ENVOY_SERVICE_SS = "ss"
+    const val ENVOY_SERVICE_SNOWFLAKE = "snowflake"
+    const val ENVOY_SERVICE_SOCKS = "socks"
+    const val ENVOY_SERVICE_UNKNOWN = "unknown"
+    const val ENVOY_ENDED_EMPTY = "empty"
+    const val ENVOY_ENDED_BLOCKED = "blocked"
+    const val ENVOY_ENDED_FAILED = "failed"
+    const val ENVOY_ENDED_TIMEOUT = "timeout"
+    const val ENVOY_ENDED_UNKNOWN = "unknown"
+
+    class Constants {}
---
+++ android/envoy/src/main/kotlin/org/greatfire/envoy/UrlUtil.kt
@@ -0,0 +1,111 @@
+package org.greatfire.envoy
+
+import android.util.Log
+import java.net.URI
+
+class UrlUtil {
+
+    companion object {
+
+        private val TAG = "UrlUtil"
+
+        fun sanitizeUrl(url: String, urlService: String): String {
+
+            var sanitizedString = ""
+            var service = urlService
+
+            // special handling for local socks urls
+            if (url.startsWith("socks5:")) {
+                service = ENVOY_SERVICE_SOCKS
+            }
+
+            // attempt to guess service for some unknowns
+            if (urlService.equals(ENVOY_SERVICE_UNKNOWN) && url.startsWith("https://")) {
+                service = ENVOY_SERVICE_HTTPS
+            } else if (urlService.equals(ENVOY_SERVICE_UNKNOWN) && url.startsWith("envoy://")) {
+                service = ENVOY_SERVICE_ENVOY
+            }
+
+            try {
+                if (service.equals(ENVOY_SERVICE_UPDATE)) {
+                    // extract number from url
+                    val parts = url.split("/")
+                    if (parts.size > 1) {
+                        sanitizedString = parts[parts.size - 2]
+                    }
+                } else if (service.equals(ENVOY_SERVICE_DIRECT)
+                    || service.equals(ENVOY_SERVICE_HTTPS)
+                ) {
+                    // extract domain from url
+                    sanitizedString = sanitizedString.plus(
+                        url.substring(
+                            url.indexOf('.') + 1,
+                            url.indexOf("/", url.indexOf('.'))
+                        )
+                    )
+                } else if (service.equals(ENVOY_SERVICE_ENVOY)) {
+                    // extract domain and ip from queries
+                    val uri = URI(url)
+                    val rawQuery = uri.rawQuery
+                    val queries = rawQuery.split("&")
+                    for (i in 0 until queries.size) {
+                        val queryParts = queries[i].split("=")
+                        if (queryParts[0].equals("url")) {
+                            if (!sanitizedString.isNullOrEmpty()) {
+                                sanitizedString = sanitizedString.plus(",")
+                            }
+                            sanitizedString = sanitizedString.plus(
+                                queryParts[1].substring(
+                                    queryParts[1].indexOf('.') + 1,
+                                    queryParts[1].indexOf("%2F", queryParts[1].indexOf('.'))
+                                )
+                            )
+                        } else if (queryParts[0].equals("address")) {
+                            if (!sanitizedString.isNullOrEmpty()) {
+                                sanitizedString = sanitizedString.plus(",")
+                            }
+                            sanitizedString = sanitizedString.plus(queryParts[1])
+                        }
+                    }
+                } else if (service.equals(ENVOY_SERVICE_SNOWFLAKE)) {
+                    // extract domain from queries
+                    val uri = URI(url)
+                    val rawQuery = uri.rawQuery
+                    val queries = rawQuery.split("&")
+                    for (i in 0 until queries.size) {
+                        val queryParts = queries[i].split("=")
+                        if (queryParts[0].equals("url")) {
+                            sanitizedString = sanitizedString.plus(
+                                queryParts[1].substring(
+                                    queryParts[1].indexOf('.') + 1,
+                                    queryParts[1].indexOf("/", queryParts[1].indexOf('.'))
+                                )
+                            )
+                        }
+                    }
+                } else {
+                    // handles v2ray/hysteria/ss and unknown services
+                    // if possible, extract prefix and port from url
+                    if (url.contains(":")) {
+                        val urlParts = url.split(":")
+                        sanitizedString = urlParts[0]
+                        if (urlParts.size == 3) {
+                            // if found, remove queries
+                            if (urlParts[2].contains("?")) {
+                                sanitizedString = sanitizedString.plus(":" + urlParts[2].split("?")[0])
+                            } else {
+                                sanitizedString = sanitizedString.plus(":" + urlParts[2])
+                            }
+                        }
+                    } else {
+                        sanitizedString = "***"
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "got exception while sanitizing url for service " + service + ": " + e.message)
+            }
+
+            return sanitizedString
+        }
+    }
+}
