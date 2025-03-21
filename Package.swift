c284ed0b25888f77c90f6c6b4e4e85cf59f94d7a
Benjamin Erhart <berhart@netzarchitekten.com>
2025-03-21 16:35:39 +0100

    Finally brought everything to from macOS 11 to macOS 12.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "GreatfireEnvoy",
-    platforms: [.iOS(.v13), .macOS(.v11)],
+    platforms: [.iOS(.v13), .macOS(.v12)],
     products: [
         .library(name: "GreatfireEnvoy", targets: ["GreatfireEnvoy"]),
     ],
