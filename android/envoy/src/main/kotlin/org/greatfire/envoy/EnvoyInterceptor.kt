eb26df7dd74d2795ed959e0db741d9ada9ade279
Steven McDonald <steven.mcdonald@gmail.com>
2025-05-02 12:21:57 -0700

    add headers from envoy:// URLs

--- android/envoy/src/main/kotlin/org/greatfire/envoy/EnvoyInterceptor.kt
+++ android/envoy/src/main/kotlin/org/greatfire/envoy/EnvoyInterceptor.kt
@@ -149,8 +149,6 @@ class EnvoyInterceptor : Interceptor {
     private fun okHttpToEnvoy(chain: Interceptor.Chain): Response {
         val origRequest = chain.request()
 
-        // XXX envoy:// URL headers
-
         Log.d(TAG, "okHttpToEnvoy: " + origRequest.url)
 
         return chain.proceed(getEnvoyRequest(origRequest))
