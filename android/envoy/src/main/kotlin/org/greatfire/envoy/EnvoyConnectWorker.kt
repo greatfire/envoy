e1980a38f7d78aca4ac1df520ffce3235f348d9b
Steven McDonald <steven.mcdonald@gmail.com>
2025-05-15 13:55:09 -0700

    MASQUE support; fix up some comments

--- android/envoy/src/main/kotlin/org/greatfire/envoy/EnvoyConnectWorker.kt
+++ android/envoy/src/main/kotlin/org/greatfire/envoy/EnvoyConnectWorker.kt
@@ -5,11 +5,16 @@ import android.net.Uri
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-// import java.net.URI
 import kotlinx.coroutines.*
 
 /*
-    Establish a connection to an Envoy Proxy
+    Establish a connection to a supported proxy
+
+    This copies the EnvoyTest objects out of EnvoyConectionTests, shuffles
+    them, and starts testing several in parallel.
+
+    The actul test methods also live in EnvoyConectionTests
+
 */
 
 class EnvoyConnectWorker(
@@ -21,7 +26,6 @@ class EnvoyConnectWorker(
 
     companion object {
         private const val TAG = "EnvoyConnectWorker"
-        // MNB retry/timeout logic moved to util class
     }
 
     // collection of all tests that need to be run
@@ -31,10 +35,10 @@ class EnvoyConnectWorker(
     private val envoyTests = ArrayDeque<EnvoyTest>()
     private val jobs = mutableListOf<Job>()
 
-    // MNB these are singletons now so no need to store them in the companion
     private val state = EnvoyState.getInstance()
     private val util = EnvoyTestUtil.getInstance()
 
+    // "Worker"
     // This is run in EnvoyNetworking.concurrency number of coroutines
     // It effectively limits the number of servers we test at a time
     suspend fun testUrls(id: Int) {
@@ -45,6 +49,7 @@ class EnvoyConnectWorker(
             val test = envoyTests.removeFirstOrNull()
             if (test == null) {
                 // Log.d(WTAG, "NO TESTS LEFT, BREAK")
+                // XXX ask for more URLs?
                 break
             }  else if (state.connected.get()) {
                 // Log.d(WTAG, "ALREADY CONNECTED, BREAK")
@@ -61,7 +66,6 @@ class EnvoyConnectWorker(
             } else {
                 // starts the timer and updates the tally
                 util.startTest(test)
-                // Log.d(WTAG, "EXECUTE TEST FOR - " + test)
             }
 
             // MNB: temp, replace test.testType string with enum?
@@ -83,13 +87,22 @@ class EnvoyConnectWorker(
                 EnvoyServiceType.CRONET_ENVOY -> {
                     tests.testCronetEnvoy(test, context)
                 }
+                EnvoyServiceType.OKHTTP_MASQUE -> {
+                    tests.testMasqueOkHttp(test)
+                }
+                EnvoyServiceType.CRONET_MASQUE -> {
+                    tests.testMasqueCronet(test, context)
+                }
                 EnvoyServiceType.OKHTTP_PROXY -> {
+                    // url is a proxy URL, so set it as the proxyUrl as well
+                    // all the other services set something in proxyUrl
+                    // so this avoids a special case in the interceptor
+                    test.proxyUrl = test.url
                     tests.testStandardProxy(proxyUri)
                 }
-                // TODO, we have all the bits, hook them up
-                // EnvoyServiceType.CRONET_PROXY -> {
-                //     tests.testCronetProxy(proxyUri)
-                // }
+                EnvoyServiceType.CRONET_PROXY -> {
+                    tests.testCronetProxy(test, context)
+                }
                 EnvoyServiceType.HTTP_ECH -> {
                     tests.testECHProxy(test)
                 }
@@ -127,7 +140,11 @@ class EnvoyConnectWorker(
             }
 
             if (res) {
+                // We found a working connection!
+
+                // Report the success
                 util.stopTestPassed(test)
+                // Use this connection if we haven't found a working on already
                 state.connectIfNeeded(test)
             } else {
                 // report test failure. failed tests will not be retried until time passes
@@ -182,14 +199,11 @@ class EnvoyConnectWorker(
         // MNB jobs have all completed, report overall status
         util.testsComplete()
 
-        Log.d(TAG, "EnvoyConnectWorker workers are done?")
+        // Log.d(TAG, "EnvoyConnectWorker workers are done?")
     }
 
     private suspend fun startEnvoy() = coroutineScope {
-        Log.d(TAG, "startEnvoy1: ${Thread.currentThread().name}")
-
         launch {
-            Log.d(TAG, "startEnvoy2: ${Thread.currentThread().name}")
             // Pick a working DoH server
             state.dns.init()
             // if one was picked, pass it over to the Go code to use
@@ -202,12 +216,10 @@ class EnvoyConnectWorker(
             Log.d(TAG, "startEnvoy3: ${Thread.currentThread().name}")
 
             // initialize the go code
-
             // should we use a subdir? This is (mostly?) used for
             // the PT state directory in Lyrebird
             state.emissary.init(context.cacheDir.path)
 
-            Log.d(TAG, "startEnvoy4: ${Thread.currentThread().name}")
             // start test workers
             // this depends (in some cases) on dns.init() completing
             startWorkers()
@@ -219,18 +231,6 @@ class EnvoyConnectWorker(
     //
     // Main entry point
     override suspend fun doWork(): Result {
-
-        envoyTests.clear()
-        jobs.clear()
-
-        // this triggers spuriously...
-        // sanity check
-        // if (EnvoyConnectionTests.envoyTests.size < 1) {
-        //     Log.d(TAG, "NOTHING TO TEST")
-        //     util.testsComplete()
-        //     return Result.success()
-        // }
-
         // test direct connection first
         if (EnvoyConnectionTests.directUrl != "") {
             val test = EnvoyTest(EnvoyServiceType.DIRECT, EnvoyConnectionTests.directUrl)
