a2117a532082dfd72c6a24a81c78aab8ea8c9591
Steven McDonald <steven.mcdonald@gmail.com>
2025-05-15 13:47:06 -0700

    remove some dead code

--- android/envoy/src/main/kotlin/org/greatfire/envoy/CronetNetworking.kt
+++ android/envoy/src/main/kotlin/org/greatfire/envoy/CronetNetworking.kt
@@ -10,16 +10,10 @@ import org.chromium.net.UrlRequest
 import java.io.File
 import java.io.IOException
 import java.lang.reflect.Field
-import java.net.URL
-import java.net.URI
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-/*
-import java.security.Provider
-import java.security.Security
- */
 
 object CronetNetworking {
     private var mCronetEngine: CronetEngine? = null
@@ -63,10 +57,8 @@ object CronetNetworking {
         resolverRules?.let {
             builder = builder.setResolverRules(it)
         }
+        // XXX TLS options here, if we support them
 
-        // if (strategy > 0) {
-        //     builder = builder.SetStrategy(strategy)
-        // }
         return builder.build()
     }
 
@@ -74,57 +66,6 @@ object CronetNetworking {
     fun cronetEngine(): CronetEngine? {
         return mCronetEngine
     }
-
-    //@JvmStatic
-    //fun setCustomCronetBuilder(builder: CustomCronetBuilder?) {
-    //    customCronetBuilder = builder
-    //}
-
-    // @JvmStatic
-    // @Synchronized
-    // @JvmOverloads
-    // fun initializeCronetEngine(context: Context, envoyUrl: String?, reInitializeIfNeeded: Boolean = false, strategy: Int = 0) {
-    //     Log.d(TAG, "try to initialize cronet engine with url $envoyUrl")
-    //     if (this.mCronetEngine != null && !reInitializeIfNeeded) {
-    //         Log.d(TAG, "cronet engine is initialized already, and reInitializeIfNeeded is $reInitializeIfNeeded")
-    //         return
-    //     }
-    //     if (mCustomCronetBuilder != null) {
-    //         mCronetEngine = mCustomCronetBuilder!!.build(context)
-    //     } else {
-    //         mCronetEngine = buildEngine(
-    //             context = context,
-    //             cacheFolder = "cronet-cache",
-    //             envoyUrl = envoyUrl,
-    //             strategy = strategy,
-    //             cacheSize = 10
-    //         )
-    //         if (mCronetEngine != null) {
-    //             Log.d(TAG, "engine version " + mCronetEngine!!.versionString)
-    //             val factory = mCronetEngine!!.createURLStreamHandlerFactory()
-    //             // https://stackoverflow.com/questions/30267447/seturlstreamhandlerfactory-and-java-lang-error-factory-already-set
-    //             try {
-    //                 // Try doing it the normal way
-    //                 URL.setURLStreamHandlerFactory(factory)
-    //             } catch (e: Error) {
-    //                 // Force it via reflection
-    //                 try {
-    //                     val factoryField: Field = URL::class.java.getDeclaredField("factory")
-    //                     factoryField.isAccessible = true
-    //                     factoryField.set(null, factory)
-    //                 } catch (ex: Exception) {
-    //                     when (ex) {
-    //                         is NoSuchFieldException, is IllegalAccessException ->
-    //                             Log.e(TAG, "Could not access factory field on URL class: {}", e)
-    //                         else -> throw ex
-    //                     }
-    //                 }
-    //             }
-    //         } else {
-    //             Log.e(TAG, "failed to initialize cronet engine")
-    //         }
-    //     }
-    // }
 
     @JvmStatic
     @Throws(IOException::class)
@@ -154,47 +95,6 @@ object CronetNetworking {
         return requestBuilder.build()
     }
 
-    /*
-        Like buildRequest, but assumes an unpatched Cronet
-    */
-    @JvmStatic
-    @Throws(IOException::class)
-    fun buildEnvoyRequest(
-        envoyUrl: String,
-        request: Request,
-        callback: UrlRequest.Callback?,
-        cronetEngine: CronetEngine,
-        executorService: ExecutorService
-    ) : UrlRequest {
-        val targetUrl = request.url.toString()
-        val targetHost = URI(targetUrl).getHost()
-
-        val requestBuilder = cronetEngine.newUrlRequestBuilder(envoyUrl, callback, executorService)
-        requestBuilder.setHttpMethod(request.method)
-        request.headers.forEach {
-            if (it.first.lowercase(Locale.ENGLISH) != "accept-encoding") {
-                requestBuilder.addHeader(it.first, it.second)
-            }
-        }
-        // set Envoy headers
-        requestBuilder.addHeader("Host-Orig", targetHost)
-        requestBuilder.addHeader("Url-Orig", targetUrl)
-
-        val requestBody = request.body
-        if (requestBody != null) {
-            val contentType = requestBody.contentType()
-            if (contentType != null) {
-                requestBuilder.addHeader("Content-Type", contentType.toString())
-            }
-            val buffer = Buffer()
-            requestBody.writeTo(buffer)
-            val uploadDataProvider = UploadDataProviders.create(buffer.readByteArray())
-            requestBuilder.setUploadDataProvider(uploadDataProvider, executorService)
-        }
-
-        return requestBuilder.build()
-    }
-
     @JvmStatic
     @Throws(IOException::class)
     fun buildRequest(request: Request, callback: UrlRequest.Callback?, cronetEngine: CronetEngine): UrlRequest {
@@ -206,9 +106,4 @@ object CronetNetworking {
     fun buildRequest(request: Request, callback: UrlRequest.Callback?): UrlRequest {
         return buildRequest(request, callback, mCronetEngine!!, mExecutorService)
     }
-
-    interface CustomCronetBuilder {
-        fun build(context: Context?): CronetEngine?
-    }
-
 }
