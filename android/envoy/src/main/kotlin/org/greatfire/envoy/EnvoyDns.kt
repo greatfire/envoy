7d09c5e5936460b8f9aa4a41f0065e62b0159ed3
Steven McDonald <steven.mcdonald@gmail.com>
2025-06-09 13:08:03 -0700

    ECH keys rotate too frequently to hardcode one :)

--- android/envoy/src/main/kotlin/org/greatfire/envoy/EnvoyDns.kt
+++ android/envoy/src/main/kotlin/org/greatfire/envoy/EnvoyDns.kt
@@ -11,7 +11,6 @@ class EnvoyDns() {
 
     companion object {
         private const val TAG = "EnvoyDns"
-        private const val CLOUDFLARE_ECH = "AEX+DQBBCwAgACBozL6Jw4kX2IzWVC3KpnxYOat5ln6RtIWLTlTAptuNAgAEAAEAAQASY2xvdWRmbGFyZS1lY2guY29tAAA="
 
         private val CLOUDFLARE_SERVERS = listOf<String>("1.1.1.1", "1.0.0.1", "[2606:4700:4700::1111]", "[2606:4700:4700::1001]")
         private val QUAD9_SERVERS = listOf<String>("9.9.9.9", "149.112.112.112", "[2620:fe::fe]", "[2620:fe::fe:9]")
@@ -123,7 +122,7 @@ class EnvoyDns() {
             } catch (e: Error) {
                 Log.e(TAG, "ECH Config lookup failed for $host $e")
                 // we're probably talking to cloudflare, so try their key
-                return CLOUDFLARE_ECH
+                return ""
             }
         }
 
@@ -131,7 +130,7 @@ class EnvoyDns() {
         val matchResult = re.find(echRecord!!)
         val ech = matchResult?.groups[1]?.value
 
-        return ech!!
+        return ech ?: ""
     }
 
     suspend fun init() {
