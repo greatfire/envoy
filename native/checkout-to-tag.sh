083f6a6adb5d4886fcbf6ad3db6bbc6fea5a34af
Steven McDonald <steven.mcdonald@gmail.com>
2025-06-25 15:32:34 -0700

    updates for 138.0.7204.35

--- native/checkout-to-tag.sh
+++ native/checkout-to-tag.sh
@@ -8,7 +8,7 @@ export DEPOT_TOOLS_UPDATE=0
 CHROMIUM_SRC_ROOT=${CHROMIUM_SRC_ROOT:-/root/chromium/src}
 DEPOT_TOOLS_ROOT=${DEPOT_TOOLS_ROOT:-/root/depot_tools}
 export PATH="$DEPOT_TOOLS_ROOT:$PATH"
-TAG=${1:-128.0.6537.2}
+TAG=${1:-138.0.7204.35}
 
 cd "$CHROMIUM_SRC_ROOT" || exit 1
 
@@ -27,6 +27,6 @@ git clean -ffd # --dry-run
 
 gclient sync --nohooks
 # Will prompt for package installation
-build/install-build-deps.sh --android
+build/install-build-deps.sh --arm --lib32 --no-nacl
 gclient runhooks
 
