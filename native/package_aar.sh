370155137f1292b01a9bfe9d5343724ef85a0418
Steven McDonald <steven.mcdonald@gmail.com>
2025-07-01 12:34:25 -0700

    cronet seems to require callers to import protobuf-javalite

--- native/package_aar.sh
+++ native/package_aar.sh
@@ -41,7 +41,7 @@ touch public.txt
 echo '<?xml version="1.0" encoding="utf-8"?>
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
     package="org.chromium.net">
-    <uses-sdk android:minSdkVersion="21" android:targetSdkVersion="24" />
+    <uses-sdk android:minSdkVersion="21" android:targetSdkVersion="34" />
     <uses-permission android:name="android.permission.INTERNET"/>
     <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>
 </manifest>' >AndroidManifest.xml
