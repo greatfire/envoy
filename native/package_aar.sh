dc28e270a5c13991132b9902b67bc8798eb7d506
Steven McDonald <steven.mcdonald@gmail.com>
2025-10-03 15:41:17 -0700

    updated shadowsocks client

--- native/package_aar.sh
+++ native/package_aar.sh
@@ -50,8 +50,9 @@ jar cvMf "cronet-$BUILD_VARIANT.aar" -C $build_tmp_dir/ .
 
 # updated Shadowsocks based on research:
 # https://www.opentech.fund/news/exposing-the-great-firewalls-dynamic-blocking-of-fully-encrypted-traffic/
-wget --continue https://github.com/gfw-report/shadowsocks-rust/releases/download/v0.0.1-beta/mobile-universal-release-signed.apk
-unzip -o mobile-universal-release-signed.apk lib/*/libsslocal.so
+#wget --continue https://github.com/gfw-report/shadowsocks-rust/releases/download/v0.0.1-beta/mobile-universal-release-signed.apk
+#unzip -o mobile-universal-release-signed.apk lib/*/libsslocal.so
+unzip -o mobile-universal-release-unsigned.apk lib/*/libsslocal.so
 rsync -avzp lib/ jni/
 jar uvMf "cronet-$BUILD_VARIANT.aar" jni
 
